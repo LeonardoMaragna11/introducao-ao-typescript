b2d1ff3cca03a61c120444520b2b2f931873bcbd
Leonardo Maragna <86269559+LeonardoMaragna11@users.noreply.github.com>
2022-05-01 11:15:31 -0300

    commit

--- index.ts
+++ index.ts
@@ -1,1 +1,37 @@
-console.log('Hello, World!');
+//      Dando Tipo para as variaveis
+/*
+    function soma(a: number, b:number){
+        return a + b
+    }
+*/
+
+//      Types e Interfaces
+
+// interface IFelino extends IAnimal{
+//     visaoNoturna:boolean,
+// }
+
+// interface IAnimal{
+//     nome:string,
+//     tipo:'terrestre' | 'aquatico',
+//     executarRugido():void
+// }
+
+// const animal:IAnimal={
+//     nome:'Elefante',
+//     tipo:'terrestre',
+//     executarRugido:()=>{console.log('Arrr');}
+// }
+
+// const felino: IFelino = {
+//     nome:'Leão',
+//     tipo:'terrestre',
+//     visaoNoturna: false,
+//     executarRugido:()=>{console.log('Miau')}
+// } 
+
+type IAnimal={
+    nome:string,
+    tipo:'terrestre' | 'aquatico',
+    executarRugido():void
+}
